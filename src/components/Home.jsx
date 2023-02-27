0252d6f28090433b8b398dbad0476e46a6a949a0
zaki358 <t.yamazaki.1129@gmail.com>
2023-02-27 23:03:15 +0900

    コミット

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from "react";
+
+import "./Home.css";
 
 export const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="homePage">
+      <div className="postContents">
+        <div className="postHeader">
+          <h1>タイトル</h1>
+        </div>
+        <div className="postTextContainer">
+          学習あああああああああああああああああああああああああ
+        </div>
+        <div className="nameAndDeletebutton">
+          <h3>@zaki</h3>
+          <button>削除</button>
+        </div>
+      </div>
+    </div>
+  );
+};
